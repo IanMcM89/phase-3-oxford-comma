deea59c8d74a8174217e42111ee7b203de9461b7
Ian <ianmcm.89@gmail.com>
2022-01-24 14:51:25 -0500

    commit tree d8fca772abdc703cd38af93740e37ad931717feb

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
 def oxford_comma(array)
-
+    if array.length == 2 then array.join(" and ")
+    elsif array.length == 3 then array.slice(0, 2).join(", ") << ", and "  << array.slice(2)
+    elsif array.length > 3 then array.slice(0, array.length - 1).join(", ") << ", and " << array.last
+    else array.join(" ,")
+    end
 end
